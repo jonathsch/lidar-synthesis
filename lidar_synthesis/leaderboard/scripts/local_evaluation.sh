7702435f975092a9a7e22a612b306f3d47637d40
Jonathan Schmidt <jonathan.schmidt@tum.de>
2023-08-08 11:45:16 +0200

    Fix CARLA import

--- lidar_synthesis/leaderboard/scripts/local_evaluation.sh
+++ lidar_synthesis/leaderboard/scripts/local_evaluation.sh
@@ -2,8 +2,8 @@ export CARLA_ROOT=/home/jonathan/CARLA-10/
 export WORK_DIR=/home/jonathan/dev/lidar-synthesis
 
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
-# export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
-# export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 export SCENARIO_RUNNER_ROOT=${WORK_DIR}/lidar_synthesis/scenario_runner
 export LEADERBOARD_ROOT=${WORK_DIR}/lidar_synthesis/leaderboard
